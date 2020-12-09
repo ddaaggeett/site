869ff869febd67232e35a5ade7997c98748c1ef6
ddaaggeett <david.g.daggett@gmail.com>
2020-12-08 21:52:24 -0500

    profile

--- src/components/NavButtons.js
+++ src/components/NavButtons.js
@@ -9,7 +9,7 @@ export default class NavButtons extends Component {
                                 <MediaQuery maxWidth={991}>
                                         <div className="mobile_vertical">
                                                 <Link to={'/'}><div className="button_mobile"><p>home</p></div></Link>
-                                                <Link to={'/projects'}><div className="button_mobile"><p>i make things</p></div></Link>
+                                                <Link to={'/projects'}><div className="button_mobile"><p>I do things</p></div></Link>
                                                 <Link to={'/contact'}><div className="button_mobile"><p>contact me</p></div></Link>
                                         </div>
                                 </MediaQuery>
@@ -17,7 +17,7 @@ export default class NavButtons extends Component {
                                 <MediaQuery minWidth={992}>
                                         <div className="mobile_vertical">
                                                 <Link to={'/'}><div className="button"><p>home</p></div></Link>
-                                                <Link to={'/projects'}><div className="button"><p>i make things</p></div></Link>
+                                                <Link to={'/projects'}><div className="button"><p>I do things</p></div></Link>
                                                 <Link to={'/contact'}><div className="button"><p>contact me</p></div></Link>
                                         </div>
                                 </MediaQuery>
